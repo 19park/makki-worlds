f3cacd6d90fbc40ea159344645bf04feab59608a
19park <19park@wholedoc.net>
2019-03-07 19:30:07 +0900

    update nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,11 +6,12 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: 'Nuxt Firebase SNS Example',
+    title: '마끼월드',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: '마끼 project' },
+      { hid: 'og:image', property: 'og:image', content: 'https://makki.ml/makki.jpg' }
     ],
     link: [
       { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' },
